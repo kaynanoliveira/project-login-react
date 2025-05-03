4024b49a6b6900158625fc93c40310c3f0e8a475
kayna <kaynanoliveira357@gmail.com>
2025-05-03 01:07:08 -0300

    Estilização do Button

---
+++ src/components/Button/styles.js
@@ -0,0 +1,33 @@
+import {styled, css} from "styled-components";
+
+const ButtonContainer = styled.button`
+    background: #565656;
+    border-radius: 22px;
+    position: relative;
+
+    color: #FFFFFF;
+    padding: 2px 12px;
+    min-width: 120px;
+    width: 100%;
+
+    ${({varient})=> varient !== "primary" && css `
+        min-width: 167px;
+        height: 33px;
+
+        background: #E41050;
+
+        &::after {
+            content: '',
+            position: absolute;
+            border: 1px solid #E41050;
+            top: -5px;
+            left: -6px;
+            width: calc(100% + 10px);
+            height: calc(100% + 10px);
+            border-radius: 22px;
+         }
+        `
+    }
+`;
+
+export {ButtonContainer}
