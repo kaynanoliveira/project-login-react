4aa3062b61f8e4b87d0fc27f0e27e85cbb9e8e28
kayna <kaynanoliveira357@gmail.com>
2025-05-07 01:02:54 -0300

    Estilizando a Home

--- src/pages/home/styles.js
+++ src/pages/home/styles.js
@@ -3,9 +3,9 @@ import styled from "styled-components";
 
 export const Container = styled.main`
     width: 100%;
-    max-width: 80%;
+    max-width: 90%;
     margin: 0 auto;
-    margin-top: 120px;
+    margin-top: 70px;
 
     display: flex;
     flex-direction: row;
@@ -18,8 +18,8 @@ export const Title = styled.h2`
     font-size: 32px;
     font-style: normal;
     font-weight: 700;
-    width: 320px;
-    margin-botton: 20px;
+    width: 400px;
+    margin-bottom: 20px;
     line-height: 44px;
 
     color: #FFFFFF;
@@ -35,8 +35,20 @@ export const TextContent = styled.p`
     font-style: normal;
     font-weight: 400;
     width: 420px;
-    margin-botton: 20px;
+    margin-bottom: 20px;
     line-height: 22px;
 
     color: #FFFFFF;
+`
+
+export const BannerImage = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    img {
+        max-width: 480px;
+        width: 100%;
+        height: auto;
+    }
 `
